bae0459bca4cfb1554058822f1d2bff609bbfcff
Nathan Essama <emperor.frost17@gmail.com>
2026-08-03 15:04:20 +0000

    loaded the code into the chatbot project folder

--- chatbot-project/src/main.jsx
+++ chatbot-project/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+  //<StrictMode gives us some additional checks and warnings when developing our app
   <StrictMode>
     <App />
   </StrictMode>,
