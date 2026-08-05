a928730fe5f3bed0ee2e5475c1cd5f20e24f81bf
Nathan Essama <emperor.frost17@gmail.com>
2026-08-05 15:39:27 +0000

    Create multiple pages using react-router

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,11 +1,21 @@
+import { Routes, Route } from "react-router";
 import { HomePage } from "./pages/HomePage";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    //This tells React all the pgaes that are in our website
+    <Routes>
+      {/*This adds a page to our website
+      It takes two props
+      1. path props is for the URL path of the page. for it to be empty just put "/" or just use the index prop it is the same
+      2. element props tells React which element or component to display e.g <HomePage />
+
+      N/B: All these Routes linked share on HTML file which is index.html
+      */}
+      <index element={<HomePage />} />
+      <Route path="checkout" element={<div>test Checkout Page</div>} />
+    </Routes>
   );
 }
 
