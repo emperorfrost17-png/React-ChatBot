a0f53bf1616d5f4511637a66d0ea70f3c2c1f077
Nathan Essama <emperor.frost17@gmail.com>
2026-08-09 14:55:53 +0000

    done with 6f

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -1,14 +1,16 @@
-import { Link } from "react-router";
+import { NavLink } from "react-router";
 import "./Header.css";
 export function Header() {
   return (
     <div className="header">
       <div className="left-section">
-        {/* Link component helps us go to another page without reloading. it is better to use this rather than using a tags because a tags reload the page */}
-        <Link to="/" className="header-link">
+        {/* NavLink component helps us go to another page without reloading. it is better to use this rather than using a tags because a tags reload the page */}
+
+        {/*But it has a special feature. The special feature is that it knows which page is loaded . For example, If you're on the Orders page, it adds a class called active to the Orders link (className="orders-link ... active"). */}
+        <NavLink to="/" className="header-link">
           <img className="logo" src="images/logo-white.png" />
           <img className="mobile-logo" src="images/mobile-logo-white.png" />
-        </Link>
+        </NavLink>
       </div>
 
       <div className="middle-section">
@@ -20,15 +22,15 @@ export function Header() {
       </div>
 
       <div className="right-section">
-        <Link className="orders-link header-link" to="/orders">
+        <NavLink className="orders-link header-link" to="/orders">
           <span className="orders-text">Orders</span>
-        </Link>
+        </NavLink>
 
-        <Link className="cart-link header-link" to="/checkout">
+        <NavLink className="cart-link header-link" to="/checkout">
           <img className="cart-icon" src="images/icons/cart-icon.png" />
           <div className="cart-quantity">3</div>
           <div className="cart-text">Cart</div>
-        </Link>
+        </NavLink>
       </div>
     </div>
   );
