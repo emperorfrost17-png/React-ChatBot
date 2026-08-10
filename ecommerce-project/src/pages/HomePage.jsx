b8682634a7465d442126f222dbd433e99372ec0e
Nathan Essama <emperor.frost17@gmail.com>
2026-08-10 15:28:39 +0000

    Use data from the backend to generate the HTML

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,18 +1,21 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 import { Header } from "../components/Header";
-import { products } from "../../starting-code/data/products";
 import "./HomePage.css";
 import CheckMarkIcon from "../assets/images/icons/checkmark.png";
 export function HomePage() {
-  //This is to get data from the backend
-  //The string is for the URL where we are fetching the data from
-  //N:B returns a Promise and a Promise lets us wait for asynchronous code to finsish
-  //when it finishes in the future it runs the function in .then()
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    //This is to get data from the backend
+    //The string is for the URL where we are fetching the data from
+    //N:B returns a Promise and a Promise lets us wait for asynchronous code to finsish
+    //when it finishes in the future it runs the function in .then()
 
-  //The difference between fetch() and axios.get() is that for axios, the data  will be saved directly into the response directly while for fetch it isn't
-  axios.get("http://localhost:3000/api/products").then((response) => {
-    console.log(response.data);
-  });
+    //The difference between fetch() and axios.get() is that for axios, the data  will be saved directly into the response directly while for fetch it isn't
+    axios.get("http://localhost:3000/api/products").then((response) => {
+      setProducts(response.data);
+    });
+  }, []);
 
   return (
     <>
