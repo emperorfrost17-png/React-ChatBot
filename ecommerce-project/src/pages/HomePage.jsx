08c3c725e251fc26ca72b2aa17e28204848d9b6a
Nathan Essama <emperor.frost17@gmail.com>
2026-08-10 15:03:33 +0000

    Fetch data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -3,6 +3,17 @@ import { products } from "../../starting-code/data/products";
 import "./HomePage.css";
 import CheckMarkIcon from "../assets/images/icons/checkmark.png";
 export function HomePage() {
+  //This is to get data from the backend
+  //The string is for the URL where we are fetching the data from
+  //N:B returns a Promise and a Promise lets us wait for asynchronous code to finsish
+  //when it finishes in the future it runs the function in .then()
+  fetch("http://localhost:3000/api/products").then((response) => {
+    //.json() gives us the data attached to the response
+    //it is also asychronous just like fetch response and also a promise
+   return response.json()
+  }).then((data) => {
+      console.log(data);
+    });;
   return (
     <>
       <title>Ecommerce Project</title>
@@ -17,10 +28,7 @@ export function HomePage() {
               //React uses the key prop to identify which items in a list have changed, been added, or removed.
               <div key={product.id} className="product-container">
                 <div className="product-image-container">
-                  <img
-                    className="product-image"
-                    src={product.image}
-                  />
+                  <img className="product-image" src={product.image} />
                 </div>
 
                 <div className="product-name limit-text-to-2-lines">
@@ -32,10 +40,14 @@ export function HomePage() {
                     className="product-rating-stars"
                     src={`images/ratings/rating-${product.rating.stars * 10}.png`}
                   />
-                  <div className="product-rating-count link-primary">{product.rating.count}</div>
+                  <div className="product-rating-count link-primary">
+                    {product.rating.count}
+                  </div>
                 </div>
 
-                <div className="product-price">${(product.priceCents / 100).toFixed(2)}</div>
+                <div className="product-price">
+                  ${(product.priceCents / 100).toFixed(2)}
+                </div>
 
                 <div className="product-quantity-container">
                   <select>
@@ -65,7 +77,6 @@ export function HomePage() {
               </div>
             );
           })}
-         
         </div>
       </div>
     </>
