9f76ec46c6d326eb839b58892513c23b60db8ff1
Nathan Essama <emperor.frost17@gmail.com>
2026-08-20 12:55:59 +0000

    Test clicking add to cart button

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,7 +1,13 @@
 import { it, expect, describe, vi } from "vitest";
-
+import axios from "axios";
 import { render, screen } from "@testing-library/react";
+// userEvent helps us simulate events like a click
+import userEvent from "@testing-library/user-event";
 import { Product } from "./Product";
+
+//This mocks the entire axios package
+//Which means when we are testing if we import axios we will get a fake version of axios
+vi.mock("axios");
 
 describe("Product component", () => {
   it("display the product details correctly", () => {
@@ -44,5 +50,36 @@ describe("Product component", () => {
       "images/ratings/rating-45.png",
     );
     expect(screen.getByText("87")).toBeInTheDocument();
+  });
+
+  it("adds a product to the cart", async () => {
+    const product = {
+      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+      rating: {
+        stars: 4.5,
+        count: 87,
+      },
+      priceCents: 1090,
+      keywords: ["socks", "sports", "apparel"],
+    };
+    //vi.fn() creates a fake function that doesn't do anything (basically a mock)
+    const loadCart = vi.fn();
+    //this renders a component in a fake web page
+    render(<Product product={product} loadCart={loadCart} />);
+
+    // userEvent helps us simulate events like a click
+    const user = userEvent.setup();
+    const addToCartButton = screen.getByTestId("add-to-cart-button");
+    //this simulates a click event
+    // i used await because .click() takes some time to proccess
+    await user.click(addToCartButton);
+    // Verify that clicking the button sends the correct product and quantity to the cart API.
+    expect(axios.post).toHaveBeenCalledWith("/api/cart-items", {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 1,
+    });
+    expect(loadCart).toHaveBeenCalled();
   });
 });
